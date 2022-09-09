0ac1de895cc62e58f59ce8b245a152f03d241e49
alvarezk <kristine_alvarez@avantiway.com>
2022-09-09 10:59:46 -0400

    arreglos_php

--- variables.php
+++ variables.php
@@ -1,5 +1,24 @@
 <?php
 
+//Notas:
+# Los nombres de las variables son sensibles a mayúsculas y minúsculas
+# Los nombres de las variables deben comenzar con una letra o un guión bajo
+# Los nombres de las variables no pueden comenzar con un número
+# Los nombres de las variables solo pueden contener caracteres alfanuméricos y guiones bajos (A-z, 0-9 y _)
+# Los nombres de las variables no pueden contener espacios
+# Los nombres de las variables no pueden ser palabras reservadas (ejemplo: $this)
+
+#Tipos de datos:
+#String : Cadena de texto
+#Integer : Número entero
+#Float : Número decimal
+#Boolean : Verdadero o falso
+#Array : Arreglo
+#Object : Objeto
+#NULL : Variable sin valor
+#Resource : Recurso
+
+
 $nombre = "Jhoset"; # String (cadena de texto)
 $numero = 10; # Integer (número entero)
 $numero_decimal = 10.5; # Float (número decimal)
